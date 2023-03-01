4b7dc7b3d73234bc5b85073bfc3fb26121ebc000
PlanetofJay <jae44dv@gmail.com>
2023-03-01 00:19:28 -0500

    fn merge, rotate

--- index.js
+++ index.js
@@ -118,5 +118,30 @@ function draw() {
 
 }
 
+function merge(arena, player) {
 
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    })
+  })
+
+}
+
+function rotate(matrix, dir) {
+
+  for(let y = 0; y < matrix.length; ++y) {
+    for(let x = 0; x < y; ++x) {
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+    }
+  }
+  if(dir > 0) {
+    matrix.forEach((row) => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+
+}
 
