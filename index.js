a66e1cfae43d6af08dfc47782d1d2ebf8b17a74a
PlanetofJay <jae44dv@gmail.com>
2023-03-01 00:14:27 -0500

    fn drawMatrix, draw

--- index.js
+++ index.js
@@ -93,5 +93,30 @@ function createPiece(type) {
       [0, 0, 0],
     ];
   }
-  
+
 }
+
+function drawMatrix(matrix, offset) {
+
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0) {
+        context.fillStyle = colors[value];
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    })
+  })
+
+}
+
+function draw() {
+
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawMatrix(arena, {x: 0, y: 0});
+  drawMatrix(player.matrix, player.pos);
+
+}
+
+
+
